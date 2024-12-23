09045a62f42f9b5fb8be01589eb9c2fcaafeb210
xealmi <maksimfed084@gmail.ru>
2024-12-23 22:31:54 +0300

    editing mangalib_parser.data.models

--- mangalib_parser/data/models.py
+++ mangalib_parser/data/models.py
@@ -9,5 +9,5 @@ USER = Model('user')
 MODELS_LIST = [MANGA, USER]
 MODELS = ['manga', 'user']
 def get_tmodel_by_model(model:str) -> Model:
-    index = MODELS.index(id)
+    index = MODELS.index(model)
     return MODELS_LIST[index]
